fcbc7c7b8b508da2c2ccb1a167736ba70841f748
Anas Asimi <anas2020cr7@gmail.com>
2024-01-11 22:31:21 +0100

    Complete Task [3. BaseModel]

--- models/base_model.py
+++ models/base_model.py
@@ -8,5 +8,22 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        current_time = datetime.now()
+        self.created_at = current_time
+        self.updated_at = current_time
+
+    def __str__(self):
+        """ __str__ """
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """ save """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ to_dict """
+        dictionary = self.__dict__
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat("T", "auto")
+        dictionary["updated_at"] = self.updated_at.isoformat("T", "auto")
+        return dictionary
