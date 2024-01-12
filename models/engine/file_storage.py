b7c89ac44a57517fc0539b0f50b38cb7ad244ea6
Anas Asimi <anas2020cr7@gmail.com>
2024-01-13 00:08:48 +0100

    create file_storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" file_storage.py """
+
+
+class FileStorage:
+    """ FileStorage """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """ all """
+        pass
+
+    def new(self, obj):
+        """ new """
+        pass
+
+    def save(self):
+        """ save """
+        pass
+
+    def reload(self):
+        """ reload """
+        pass
