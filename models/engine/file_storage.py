539130974a36dd457dc52e0509188384bbaa5b63
Anas Asimi <anas2020cr7@gmail.com>
2024-01-15 11:19:26 +0100

    improve file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,19 +11,19 @@ class FileStorage:
 
     def all(self):
         """ all """
-        return (FileStorage.__objects)
+        return (self.__class__.__objects)
 
     def new(self, obj):
         """ new """
-        FileStorage.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+        self.__class__.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
         """ save """
-        if FileStorage.__file_path != None:
-            with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+        if self.__class__.__file_path != None:
+            with open(self.__class__.__file_path, 'w', encoding='utf-8') as f:
                 temp = {}
-                for key in FileStorage.__objects.keys():
-                    temp[key] = FileStorage.__objects[key].to_dict()
+                for key in self.__class__.__objects.keys():
+                    temp[key] = self.__class__.__objects[key].to_dict()
                 json_string = json.dumps(temp)
                 f.write(json_string)
 
@@ -31,11 +31,11 @@ class FileStorage:
         """ reload """
         from models.base_model import BaseModel
 
-        FileStorage.__objects = {}
-        if FileStorage.__file_path == None:
+        self.__class__.__objects = {}
+        if self.__class__.__file_path == None:
             return ([])
         try:
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__class__.__file_path, 'r') as f:
                 json_string = f.read()
                 if json_string is None or len(json_string) == 0:
                     return ([])
@@ -43,6 +43,6 @@ class FileStorage:
                 for obj_id in dictionary.keys():
                     if "BaseModel" in obj_id:
                         BaseModel(**dictionary[obj_id])
-            return (FileStorage.__objects)
+            return (self.__class__.__objects)
         except FileNotFoundError as err:
             return ([])
